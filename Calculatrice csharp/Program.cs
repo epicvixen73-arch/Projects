969258177f68d1c864d427b1aa4cb035fe6913df
epicvixen73-arch <epicvixen73@gmail.com>
2026-06-22 10:20:54 +0200

    Add a minigame in csharp

---
+++ Calculatrice csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Random;
+namespace Juste_prix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random al = new Random();
+            int aleatoire = al.Next(0, 101);
+            Console.WriteLine(aleatoire);
+        }
+    }
+}
