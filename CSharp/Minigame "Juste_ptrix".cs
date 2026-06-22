49d4af21ca31279a2a719dc14424896d7b62e4fb
epicvixen73-arch <epicvixen73@gmail.com>
2026-06-22 11:35:02 +0200

    Update Minigame "Juste_ptrix".cs

--- CSharp/Minigame "Juste_ptrix".cs
+++ CSharp/Minigame "Juste_ptrix".cs
@@ -8,19 +8,60 @@ namespace Juste_prix
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Random al = new Random();
             int aleatoire = al.Next(0, 51);
-            int UserGuess = 5;
+            int UserGuess;
+            int NbGuess = 5;
+            string Credit = "Fin du jeu, merci d'y avoir joué ! ";
+            int Score = 0; //0 = perdu; 1 = Gagné
             Console.WriteLine(aleatoire);
-            while (UserGuess > 0) ;
+            while (NbGuess > 0 && Score ==0)
             {
-                string message = "Ton guess: ";
-
-                if (UserGuess == aleatoire - 1 || UserGuess == aleatoire || UserGuess == aleatoire + 1) ;
+                if (Saisir("Ton guess: ", out int Guess, aleatoire)) //mettre aleatoire en `al.Next(0, 51)` mais en `aleatoire` pour débugger
                 {
-
+                    Score = 1;
+                    Console.WriteLine(Credit);
+                    continue;
+                }
+                else if (NbGuess==0 || Score == 1)
+                {
+                    Console.WriteLine(Credit);
+                    break;
+                }
+                NbGuess += -1;
+                Console.WriteLine(NbGuess + " / 5 essai(s) restant(s). ");
+            }
+        }
+        static bool IsInBorne(float UserGuess, float aleatoire)
+        {
+            if (UserGuess == aleatoire - 1 || UserGuess == aleatoire || UserGuess == aleatoire + 1)
+            {
+                global::System.Console.WriteLine("Choix valide !");
+                return true;
+            }
+            else
+            {
+                global::System.Console.WriteLine("Choix invalide !");
+                return false;
+            }
+        }
+        static bool Saisir(string message, out int UserGuess, int aleatoire = int.MaxValue)
+        {
+            Console.Write("\n" + message);
+            while (true)
+            {
+                string saisi = Console.ReadLine();
+                if (int.TryParse(saisi, out UserGuess))
+                {
+                    return IsInBorne(UserGuess, aleatoire);
+                }
+                else
+                {
+                    Console.WriteLine("Invalide, reboot... ");
+                    return false;
                 }
             }
         }
